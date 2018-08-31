1ff53068d832aa7295a1424fd53cecd1e29b876e
zhanghaitao <lroyd-zhang@systechn.com>
2018-08-30 23:48:08 -0700

    VIM

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "正在安装请耐心等待"
+if which apt-get >/dev/null; then
+	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+elif which yum >/dev/null; then
+	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
+fi
+
+##Add HomeBrew support on  Mac OS
+if which brew >/dev/null;then
+    echo "You are using HomeBrew tool"
+    brew install vim ctags git astyle
+fi
+
+sudo easy_install -ZU autopep8 
+sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
+mv -f ~/vim ~/vim_old
+cd ~/ && git clone https://github.com/lroyd/vimIDE.git
+mv -f ~/.vim ~/.vim_old
+mv -f ~/vim ~/.vim
+mv -f ~/.vimrc ~/.vimrc_old
+mv -f ~/.vim/.vimrc ~/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+echo "Lroyd正在努力为您安装bundle程序" > lroyd
+echo "安装完毕将自动退出" >> lroyd
+echo "请耐心等待" >> lroyd
+vim lroyd -c "BundleInstall" -c "q" -c "q"
+rm lroyd
+echo "安装完成"
