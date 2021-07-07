c3b017afbfebd7f25f1969a2977b6d0d7dd3b23e
lroyd <36869912+lroyd@users.noreply.github.com>
2021-07-07 18:00:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ mv -f ~/.vim ~/.vim_old
 mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Lroyd正在努力为您安装bundle程序" > lroyd
 echo "安装完毕将自动退出" >> lroyd
 echo "请耐心等待" >> lroyd
